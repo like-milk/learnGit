483f4027fd694e0d85c544800a136057e2bfb6dc
like-milk <13927258029@163.com>
2020-08-20 16:29:44 +0800

    add swift file

---
+++ testGit/testGit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  testGit
+//
+//  Created by LedvanceAppTeam on 2020/8/20.
+//  Copyright © 2020 LedvanceAppTeam. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
